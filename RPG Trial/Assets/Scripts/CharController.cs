49b126f5c04a6be2029b713695be373fdd6242ca
MadeOfDye <kevin0darius@gmail.com>
2021-02-16 23:46:55 +0200

    Beggining

---
+++ RPG Trial/Assets/Scripts/CharController.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour
+{
+    private Transform cam;
+    private void Start()
+    {
+        cam = Camera.main.transform;
+        boxCol = GetComponent<BoxCollider>();
+    }
+
+    private void Update()
+    {
+        Debug.DrawLine(transform.position, transform.position + new Vector3(0, height, 0),Color.red,1);
+        CheckGround();
+        Gravity();
+        GetInputs();
+        CalculateDirection();
+        TehJump();
+        if (Mathf.Abs(_forward)<1 && Mathf.Abs(_sideways)<1)
+        {
+            return;
+        }
+        Rotation();
+        CalculatingForward();
+        
+        Movement();
+        TheCollision();
+    }
+
+    //Setting the raw input values into float Variables
+   private float _forward;
+    private float _sideways;
+    void GetInputs()
+    {
+        _forward = Input.GetAxisRaw("Vertical");
+        _sideways = Input.GetAxisRaw("Horizontal");
+    }
+
+    //Calculate the orientation of the character(from the above perspective) dependent on the camera angle
+    private float _angle;
+    void CalculateDirection()
+    {
+        _angle = Mathf.Atan2(_sideways, _forward);
+        _angle = Mathf.Rad2Deg * _angle;
+        _angle += cam.eulerAngles.y;
+    }
+    //Rotate the player dependand on the direction 
+    public float smoothRot = 10f;
+    private Quaternion playerRot;
+    void Rotation()
+    {
+        playerRot = Quaternion.Euler(0, _angle, 0);
+        transform.rotation = Quaternion.Lerp(transform.rotation, playerRot, smoothRot*Time.deltaTime);
+    }
+    //Calculating the forward vector for slopes
+    private Vector3 forward;
+    private RaycastHit hit;
+    void CalculatingForward()
+    {
+        if(!grounded)
+        {
+            forward = transform.forward;
+            return;
+        }
+        else
+        {
+            Ray ray = new Ray(transform.TransformPoint(liftPoint), Vector3.down);
+            Physics.SphereCast(ray, 0.3f,out hit, groundPoint, eButPlayer);
+            if (Vector3.Cross(hit.normal, -transform.right).y < 0.65f) 
+            {
+                forward = Vector3.Cross(hit.normal, -transform.right);
+            }
+            else
+            {
+               forward = Vector3.zero;
+            }
+        }
+    }
+    //this is the movement part, where the actual movement happens
+    public float crackSpeed  = 10f;
+   private Vector3 movement;
+    void Movement()
+    {
+        movement = forward * crackSpeed * Time.deltaTime;
+        transform.position += movement;
+    }
+    //Setting the boolean and applying gravity
+   private bool grounded = false;
+    public float gravity = 12.5f;
+    void Gravity()
+    {
+        if(grounded == false)
+        {
+            transform.position -= new Vector3(0, gravity * Time.deltaTime, 0);
+            crackSpeed /= 2.5f;
+        }
+        else
+        {
+            crackSpeed = 10f;
+            return;
+        }
+    }
+    //checking and confirming solid objects beneath the player
+    public float height = 2f;
+    public Vector3 liftPoint = new Vector3(0, 1f, 0);  
+    public LayerMask eButPlayer;
+    public float groundPoint = 5f;
+    void CheckGround()
+    {
+        
+        Ray ray = new Ray(transform.TransformPoint(liftPoint), Vector3.down);
+        RaycastHit tempHit = new RaycastHit();
+        if (Physics.SphereCast(ray, 0.3f,out tempHit,groundPoint, eButPlayer))
+        {
+            GroundConfirm(tempHit);
+        }
+        else
+        {
+            grounded = false;
+        }
+    }
+    //Confirming that I am standing on the ground not clipping 
+    private Vector3 groundCheckPoint = new Vector3(0, -0.57f, 0);
+    public float groundCheckRadius = 1f;
+    private RaycastHit groundHit;
+    public float smoothFall = 10f;
+    void GroundConfirm(RaycastHit temphit)
+    {
+        Collider[] col = new Collider[3];
+        int num = Physics.OverlapSphereNonAlloc(transform.TransformPoint(groundCheckPoint),groundCheckRadius, col,eButPlayer);
+        //grounded = false;
+        for(int i=0;i<num;i++)
+        {
+            if(col[i] == temphit.collider)
+            {
+                groundHit = temphit;
+                grounded = true;
+                if (jumped == false)
+                {
+                    transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, (groundHit.point.y + height / 2), transform.position.z), smoothFall * Time.deltaTime);
+                    Debug.Log("Here");
+                }
+                break;
+            }
+        }
+
+    }
+    //yes I am programming the collision by hand, no it doesn't work as intended, what of it?
+    BoxCollider boxCol;
+    void TheCollision()
+    {
+        Collider[] overlaps = new Collider[4];
+        int num = Physics.OverlapBoxNonAlloc(transform.TransformPoint(boxCol.center), boxCol.size / 2, overlaps, transform.rotation, eButPlayer, QueryTriggerInteraction.UseGlobal);
+        for(int i=0;i<num;i++)
+        {
+            //Transform t = overlaps[i].transform;
+            Vector3 dir;
+            float dist;
+            if (Physics.ComputePenetration(boxCol, transform.position, transform.rotation, overlaps[i], overlaps[i].transform.position, overlaps[i].transform.rotation, out dir, out dist))
+            {
+                Vector3 penetration = dir * dist;
+                Vector3 movementProjected = Vector3.Project(movement, -dir);
+                transform.position = transform.position + penetration;
+                     movement -= movementProjected;
+            }
+        }
+    }
+    //Leave the ground and go up in the air hopefully
+   private bool jumped = false;
+    public float jumpForce = 0.6f;
+    public float jumpSpeed = 0.5f;
+    private void TehJump()
+    {
+       bool canJump = false;
+        if(grounded)
+        {
+            jumped = false;
+        }
+        canJump = !Physics.Raycast(new Ray(transform.position, Vector3.up), height, eButPlayer);
+        if(grounded && canJump)
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.position =Vector3.Lerp(transform.position,transform.position +  (Vector3.up * jumpForce * height),jumpSpeed * Time.deltaTime);
+                jumped = true;
+        }
+
+    }
+}
---
+++ RPG Trial/Assets/Scripts/Test.cs
@@ -0,0 +1,287 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+	/*public Transform lookAt;
+
+
+	Transform camTransform;
+	Quaternion looks;
+	Vector2 rotator;
+
+	private float currentX = 0.0f;
+	private float currentY = 0.0f;
+
+	public float distance = 5.0f;
+	public float sensivityX = 4.0f;
+	public float sensivityY = 1.0f;
+	public const float minClamp = -8.0f;
+	public const float maxClamp = 70.0f;
+	public float height = 1.75f;
+	public float smoothness = 2.0f;
+
+	private void Start()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+		camTransform = transform;
+	}
+	private void Update()
+	{
+		GetInputs();
+		Clamp();
+		LookAt();
+	}
+	private void LateUpdate()
+	{
+		Vector3 offset = new Vector3(0, height, -distance);
+		Quaternion rotation = Quaternion.Euler(rotator.y, rotator.x, 0);
+		camTransform.position = lookAt.position + rotation * offset;
+		//camTransform.LookAt(lookAt.position);
+	}
+	private void Clamp()
+	{
+		if (currentY > maxClamp)
+			currentY = maxClamp;
+		else if (currentY < minClamp)
+			currentY = minClamp;
+		return;
+	}
+	void GetInputs()
+	{
+		currentX += Input.GetAxis("Mouse X");
+		currentY -= Input.GetAxis("Mouse Y");
+		rotator = new Vector2(currentX, currentY);
+		rotator = Vector2.Scale(rotator, new Vector2(sensivityX, sensivityY));
+	}
+	private void LookAt()
+	{
+		looks = Quaternion.LookRotation(lookAt.position - camTransform.position);
+		camTransform.rotation = Quaternion.Lerp(camTransform.rotation, looks, smoothness);
+	}*/
+	#region Public_Variables_and_References
+	public float playerHeight = 2f;
+	public float heightPadding = 0.2f;
+	public float smoothness = 0.5f;
+	public float moveSpeed = 10.0f;
+	public float gravity = 2.5f;
+	public float groundHitCheck;
+	public bool smooth;
+	public float smoothSpeed;
+	public LayerMask discludePlayer;
+	public BoxCollider boxCol;
+	Quaternion thisRot;
+	//Rigidbody rb;
+	Camera cam;
+	public float groundCheckRadius;
+	#endregion
+	#region Main_Methods
+	void Start()
+	{
+		//rb = GetComponent<Rigidbody>();
+		cam = Camera.main;
+		thisRot = transform.rotation;
+
+	}
+
+	void Update()
+	{
+		Gravity();
+		GetInput();
+		CalculateDirection();
+		Move();
+		GroundChecking();
+		CollisionCheck();
+		DrawDebugLines();
+
+	}
+	#endregion
+	#region Movement
+	private float forw;
+	private float side;
+	void GetInput()
+	{
+		side = Input.GetAxisRaw("Horizontal");
+		forw = Input.GetAxisRaw("Vertical");
+	}
+	private float angle;
+	void CalculateDirection()
+	{
+		angle = Mathf.Atan2(side, forw);
+		angle = Mathf.Rad2Deg * angle;
+		angle += cam.transform.eulerAngles.y;
+	}
+	Quaternion playerRotation;
+	private Vector3 forward;
+	void CalculateForward()
+	{
+		if (!grounded)
+		{
+			forward = transform.forward;
+			return;
+		}
+		//universal transform.forward in 3d graphics
+		forward = Vector3.Cross(groundHit.normal, -transform.right);
+	}
+	void Rotate()
+	{
+		playerRotation = Quaternion.Euler(0, angle, 0);
+		transform.rotation = Quaternion.Lerp(transform.rotation, playerRotation, smoothness * Time.deltaTime);
+	}
+	//Vector3 playerPosition;
+	Vector3 moveVectr;
+	void Move()
+	{
+		transform.position += new Vector3(0, -currentGravity, 0);
+		if (Mathf.Abs(side) < 1 && Mathf.Abs(forw) < 1) return;
+		CalculateForward();
+		Rotate();
+		moveVectr = forward * moveSpeed * Time.deltaTime;
+		transform.position += moveVectr;
+		//playerPosition =transform.forward * moveSpeed * Time.deltaTime;
+		//rb.MovePosition(rb.position + playerPosition);
+	}
+	#endregion
+	#region Gravity
+	private bool grounded;
+	private float currentGravity = 0f;
+	private void Gravity()
+	{
+		if (grounded == false)
+		{
+			currentGravity = gravity;
+		}
+		else
+		{
+			currentGravity = 0;
+		}
+	}
+
+	private Vector3 liftPoint = new Vector3(0, 2.2f, 0);
+	private RaycastHit groundHit;
+
+
+	private void GroundChecking()
+	{
+
+		Ray ray = new Ray(transform.TransformPoint(liftPoint), Vector3.down);
+		RaycastHit tempHit = new RaycastHit();
+		if (Physics.SphereCast(ray, 0.17f, out tempHit, groundHitCheck, discludePlayer))
+		{
+			GroundConfirm(tempHit);
+		}
+		else
+		{
+			grounded = false;
+		}
+	}
+	private Vector3 groundCheckPoint = new Vector3(0, -0.57f, 0);
+	private void GroundConfirm(RaycastHit tempHit)
+	{
+		//	float currentSlope = Vector3.Angle(tempHit.normal, Vector3.up);
+		Collider[] col = new Collider[3];
+		int num = Physics.OverlapSphereNonAlloc(transform.TransformPoint(groundCheckPoint), groundCheckRadius, col, discludePlayer);
+		grounded = false;
+		for (int i = 0; i < num; i++)
+		{
+			if (col[i].transform == tempHit.transform)
+			{
+				groundHit = tempHit;
+				grounded = true;
+				if (!smooth)
+				{
+					transform.position = new Vector3(transform.position.x, (groundHit.point.y + playerHeight / 2), transform.position.z);
+				}
+				else
+				{
+					transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, (groundHit.point.y + playerHeight / 2), transform.position.z), smoothSpeed * Time.deltaTime);
+				}
+
+				break;
+			}
+		}
+		if (num <= 1 && tempHit.distance <= 3.1f)
+		{
+			if (col[0] != null)
+			{
+				Ray ray = new Ray(transform.TransformPoint(liftPoint), Vector3.down);
+				RaycastHit hit;
+				if (Physics.Raycast(ray, out hit, 3.1f, discludePlayer))
+				{
+					if (hit.transform != col[0].transform)
+					{
+						grounded = false;
+						return;
+					}
+				}
+			}
+		}
+	}
+	#endregion
+	#region Collision
+	private void CollisionCheck()
+	{
+		Collider[] overlaps = new Collider[4];
+		int num = Physics.OverlapBoxNonAlloc(transform.TransformPoint(boxCol.center), (boxCol.size / 2), overlaps, thisRot, discludePlayer, QueryTriggerInteraction.UseGlobal);
+		for (int i = 0; i < num; i++)
+		{
+			Transform t = overlaps[i].transform;
+			Vector3 dir;
+			float dist;
+			if (Physics.ComputePenetration(boxCol, transform.position, transform.rotation, overlaps[i], t.position, t.rotation, out dir, out dist)) ;
+			Vector3 penetrateVector = dir * dist;
+			Vector3 velocityProjected = Vector3.Project(moveVectr, -dir);
+			transform.position = transform.position + penetrateVector;
+			moveVectr -= velocityProjected;
+		}
+	}
+	#endregion
+	void DrawDebugLines()
+	{
+		Debug.DrawLine(transform.position, transform.position + forward * playerHeight * 2, Color.blue);
+		Debug.DrawLine(transform.position, transform.position - Vector3.up, Color.green);
+	}
+/*	readonly string axisX = "Mouse X", axisY = "Mouse Y";
+	float mouseX, mouseY;
+	public float rotationSpeedX, rotationSpeedY;
+	[Range(0, 5)]
+	public float smoothing;
+	public float lowClamp, highClamp;
+	Vector2 position, calculate, finale;
+	public Transform Player, Target;
+
+	private void Start()
+	{
+
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	void Update()
+	{
+		CamControl();
+	}
+
+	void CamControl()
+	{
+		mouseX = Input.GetAxisRaw(axisX);
+		mouseY = Input.GetAxisRaw(axisY);
+
+		position = new Vector2(mouseX, mouseY);
+		position = Vector2.Scale(position, new Vector2(rotationSpeedY + smoothing, rotationSpeedX * smoothing));
+
+		calculate.x = Mathf.LerpAngle(calculate.x, position.x, 1f / smoothing);
+		calculate.y = Mathf.LerpAngle(calculate.y, position.y, 1f / smoothing);
+		finale += calculate;
+		finale.y = Mathf.Clamp(finale.y, lowClamp, highClamp);
+
+		transform.LookAt(Target);
+
+		Target.rotation = Quaternion.Euler(finale.y, finale.x, 0);
+		Player.rotation = Quaternion.Euler(0, finale.x, 0);
+
+
+	}
+*/
+
+}

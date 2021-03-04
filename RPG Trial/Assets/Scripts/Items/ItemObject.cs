a40dcdba3782758256f8ef28d73d734e9548b656
MadeOfDye <kevin0darius@gmail.com>
2021-03-04 10:48:07 +0200

    Small Optimizations

---
+++ RPG Trial/Assets/Scripts/Items/ItemObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Type
+{
+    Food,
+    Equipment,
+    Miscellaneous
+}
+
+public abstract class ItemObject : ScriptableObject
+{
+    public GameObject prefab;
+}
---
+++ RPG Trial/Assets/Scripts/Managers/ManagerialTroubles.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManagerialTroubles : MonoBehaviour 
+{
+   
+  public  static void LoadNextScene()
+    {
+        //Scene obScene = SceneManager.GetSceneByName("TestLevel");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+       // SceneManager.MoveGameObjectToScene(GameObject.Find("Player"), obScene);
+    }
+    public static void LoadPreviousScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public static void LoadHubScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+        
+}
